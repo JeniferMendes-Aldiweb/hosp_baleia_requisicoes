2fefaa2cb5a154168bf006b8028ee85e06752945
JeniferMendes-Aldiweb <jenifer.mendes@aldiweb.com.br>
2025-09-04 09:59:03 -0300

    diagramas

---
+++ mechanisms/mecAtr_consGerOriDest.js
@@ -0,0 +1,9 @@
+function resolve(process,colleague){
+
+	var userList = new java.util.ArrayList();
+	var usuAtual = getValue("WKUser");
+	userList.add(usuAtual);
+
+	return userList;
+
+}
---
+++ workflow/scripts/transferencia.servicetask14.js
@@ -0,0 +1,2 @@
+function servicetask14(attempt, message) {
+}
